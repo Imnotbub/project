364d906a5969ce727da10aeb091b1b01977adfdd
Imnotbub <warframeelitestyle@gmail.com>
2025-03-27 01:08:43 -0400

    Added Player to Level 1

--- My project/Assets/Scripts/BarrelRiser.cs
+++ My project/Assets/Scripts/BarrelRiser.cs
@@ -16,11 +16,13 @@ public class BarrelRiser : MonoBehaviour
         
         if (Input.GetKey(riseKey))
         {
+            Mathf.Clamp(0f, 85f, 0f);
             Barrel.transform.Rotate(Vector3.left * BarrelMoveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(lowerKey))
         {
             Barrel.transform.Rotate(Vector3.right * BarrelMoveSpeed * Time.deltaTime);
+            Mathf.Clamp(0f, -30f, 0f);
         }
     }
 }
